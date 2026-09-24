427ca46b037f0471769d764a8c8f2b9c6b93322a
SUYASH-27-UI <suyashsurve30@gmail.com>
2026-09-24 13:51:19 +0530

    Create sorted_function.py

---
+++ sorted_function.py
@@ -0,0 +1,8 @@
+numbers = [40, 10, 30, 50, 20]
+
+print("Original list:", numbers)
+
+sorted_numbers = sorted(numbers)
+
+print("Sorted list:", sorted_numbers)
+print("Original list after sorting:", numbers)
